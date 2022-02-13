fdd804c82b1414d2414d7a030ef4c51a207e3cf6
Thomas Fagot <thomasfagot39@gmail.com>
2022-02-13 21:57:40 +0100

    [chore] API : cleanup

--- api/src/Repository/NetworkRepository.php
+++ api/src/Repository/NetworkRepository.php
@@ -18,33 +18,4 @@ class NetworkRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Network::class);
     }
-
-    // /**
-    //  * @return Network[] Returns an array of Network objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('n.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Network
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
